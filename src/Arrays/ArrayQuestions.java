82931c55ccbe8fb2a722f91e1396a46ce60a5849
tanishagoyal2 <tanisha.goyal.928@gmail.com>
2021-06-08 22:57:42 +0530

    swastik question

--- src/Arrays/ArrayQuestions.java
+++ src/Arrays/ArrayQuestions.java
@@ -89,8 +89,8 @@ public class ArrayQuestions {
 	}
 	
 	public static void BuyAndSell(int arr[],int n) {
-		int buy;
-		int sell;
+		int buy=0;
+		int sell=0;
 		for(int i=0;i<n-1;i++) {
 			if(arr[i]<arr[i+1]) {
 				buy=i;
--- src/dynamic/StairCase.java
+++ src/dynamic/StairCase.java
@@ -1,5 +1,4 @@
 package dynamic;
-import java.util.*;
 public class StairCase {
 
 	
