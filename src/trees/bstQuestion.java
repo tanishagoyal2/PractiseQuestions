f304a822299a7a3b2c61c6e1e1444c3bedb839af
tanishagoyal2 <tanisha.goyal.928@gmail.com>
2021-06-12 21:05:40 +0530

    delete the nodes

--- src/trees/bstQuestion.java
+++ src/trees/bstQuestion.java
@@ -2,10 +2,14 @@ package trees;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class bstQuestion {
     Node root;
     int sum=0;
+
+    //Replace each node with the sum of all greater nodes in a given BST
+    //Print level order BST.
     public void insert(int d){
         if(root==null){
             root=new Node(d);
@@ -31,6 +35,7 @@ public class bstQuestion {
             System.out.println("parent of "+d+"is "+prev.data);
         }
     }
+    
     public void adjustSum(Node node){
         if(node==null){
             return;
@@ -65,19 +70,96 @@ public class bstQuestion {
             }
         }
     }
-    public static void main(String args[]){
-        bstQuestion bst=new bstQuestion();
-        bst.insert(4);
-        bst.insert(2);
-        bst.insert(5);
-        bst.insert(3);
-        bst.insert(1);
-        bst.adjustSum(bst.root);
-        bst.BFSTraversal();
+   
+    //You are given an array of integers. First of all ,
+    // You have to form a binary search tree from given integers. 
+    //Now you have given a range. You need to remove all nodes of the BST which are outside the given range.
+    public void deleteNode(Node node,int key){
+		Node curr=node;
+		Node parent=null;
+		while(curr!=null && curr.data!=key){
+			parent=curr;
+			if(curr.data<key){
+				curr=curr.right;
+			}
+			else{
+				curr=curr.left;
+			}
+		}
+		//if node to be deleted is the leaf node
+		if(curr.left==null && curr.right==null){
+			if(parent.left==curr){
+				parent.left=null;
+			}
+			else{
+				parent.right=null;
+			}
+		}
+		else if(curr.left!=null && curr.right!=null){
+			//find the inorder successor
+			Node successor=getMinimumKey(curr);
+			int val=successor.data;
+			//delete the successor node
+			deleteNode(root,successor.data );
+			//replace it with the deleting node
+			curr.data=val;
+		}
+		else{
+			Node child=curr.left!=null?curr.left:curr.right;
+			if(curr!=root){
+				if(curr==parent.left){
+					parent.left=child;
+				}
+				else{
+					parent.right=child;
+				}
+			}
+			else{
+				root=child;
+			}
+		}
+		System.out.println("deletion completed");
+	}
+	
+	public static Node getMinimumKey(Node curr)
+    {
+        while (curr.left != null) {
+            curr = curr.left;
+        }
+        return curr;
+    }
+    public void printpreorder(Node start) {
+		if(start==null) {
+			return;
+		}
+		
+		System.out.print(start.data+"  ");
+		printpreorder(start.left);
+		printpreorder(start.right);
+	}
 
+    public static void main(String args[]){
+       int n=7;
+       int array[]={6,-13,14,-8,15,13,7};
+       int lower=-10;
+       int higher=13;
+       bstQuestion bst=new bstQuestion();
+            for(int j=0;j<n;j++){
+                bst.insert(array[j]);
+            }
+            System.out.print("PREORDER : ");
+            bst.printpreorder(bst.root);
+            for(int j=0;j<n;j++){
+                if(array[j]<lower || array[j]>higher){
+                    bst.deleteNode(bst.root, array[j]);
+                }
+            }
+            System.out.print("PREORDER : ");
+            bst.printpreorder(bst.root);
     }
     
 }
+
 class Node{
     int data;
     Node left;
